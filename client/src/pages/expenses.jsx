62d95202af2d701c9e8d3397cdc581fd7bef14d8
Alex <fajartin.alex@gmail.com>
2024-05-01 10:32:18 +0800

    expenses page

--- client/src/pages/expenses.jsx
+++ client/src/pages/expenses.jsx
@@ -1,9 +1,125 @@
 import React from 'react'
+import UserBarChart  from '../components/charts/userBarChart';
 
 function expenses() {
   return (
-    <div className='flex items-center justify-center bg-red-50 p-5 w-full'>
-    <h1 className='text-black font-bold text-xl text-center'>this is expense page</h1>
+    <div className=' p-9 w-full flex flex-col gap-6'>
+
+              <div className='p-3 border rounded-xl bg-white' style={{ height: 500 }}>
+                    <UserBarChart />
+              </div>
+
+
+              <div className='grid grid-cols-1 lg:grid-cols-3 gap-9'>
+
+
+                          <div className='flex flex-col gap-6'> 
+                                <p className='font-bold'>Necessities</p>
+
+                                <div className='p-3 border rounded-xl'>
+                                          <div className='flex flex-row items-center justify-between font-medium'>
+                                              <p>Grocery</p>
+                                              <p>04/26/2024</p>
+                                          </div>
+                                          <div className='flex flex-row items-center justify-between font-light'>
+                                              <p>Budget: 10000</p>
+                                              <p>Expense: 1000</p>
+                                          </div>
+                                </div>
+
+                                <div className='p-3 border rounded-xl'>
+                                          <div className='flex flex-row items-center justify-between font-medium'>
+                                              <p>Grocery</p>
+                                              <p>04/26/2024</p>
+                                          </div>
+                                          <div className='flex flex-row items-center justify-between font-light'>
+                                              <p>Budget: 10000</p>
+                                              <p>Expense: 1000</p>
+                                          </div>
+                                </div>
+
+                                <div className='p-3 border rounded-xl'>
+                                          <div className='flex flex-row items-center justify-between font-medium'>
+                                              <p>Grocery</p>
+                                              <p>04/26/2024</p>
+                                          </div>
+                                          <div className='flex flex-row items-center justify-between font-light'>
+                                              <p>Budget: 10000</p>
+                                              <p>Expense: 1000</p>
+                                          </div>
+                                </div>
+
+                                <div className='p-3 border rounded-xl'>
+                                          <div className='flex flex-row items-center justify-between font-medium'>
+                                              <p>Grocery</p>
+                                              <p>04/26/2024</p>
+                                          </div>
+                                          <div className='flex flex-row items-center justify-between font-light'>
+                                              <p>Budget: 10000</p>
+                                              <p>Expense: 1000</p>
+                                          </div>
+                                </div>
+
+                                <div className='p-3 border rounded-xl'>
+                                          <div className='flex flex-row items-center justify-between font-medium'>
+                                              <p>Grocery</p>
+                                              <p>04/26/2024</p>
+                                          </div>
+                                          <div className='flex flex-row items-center justify-between font-light'>
+                                              <p>Budget: 10000</p>
+                                              <p>Expense: 1000</p>
+                                          </div>
+                                </div>
+
+
+
+                          </div>
+
+                          <div className='flex flex-col gap-6'> 
+                                <p className='font-bold'>Savings</p>
+
+                                <div className='p-3 border rounded-xl'>
+                                          <div className='flex flex-row items-center justify-between font-medium'>
+                                              <p>Grocery</p>
+                                              <p>04/26/2024</p>
+                                          </div>
+                                          <div className='flex flex-row items-center justify-between font-light'>
+                                              <p>Budget: 10000</p>
+                                              <p>Expense: 1000</p>
+                                          </div>
+                                </div>
+
+
+
+                          </div>
+
+                          <div className='flex flex-col gap-6'> 
+                                <p className='font-bold'>Wants</p>
+
+                                <div className='p-3 border rounded-xl'>
+                                          <div className='flex flex-row items-center justify-between font-medium'>
+                                              <p>Grocery</p>
+                                              <p>04/26/2024</p>
+                                          </div>
+                                          <div className='flex flex-row items-center justify-between font-light'>
+                                              <p>Budget: 10000</p>
+                                              <p>Expense: 1000</p>
+                                          </div>
+                                </div>
+
+
+
+                          </div>
+
+
+
+
+
+
+              </div>
+
+
+      
     </div>
   )
 }
