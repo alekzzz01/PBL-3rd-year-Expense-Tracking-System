2f04c121d0df52512815a89a7d145d1b313024e0
Alex <fajartin.alex@gmail.com>
2024-05-01 20:32:46 +0800

    aa

--- server/routes/expense-routes.js
+++ server/routes/expense-routes.js
@@ -4,7 +4,7 @@ import authMiddleware from '../middleWare/checkAuth.js';
 
 const router = express.Router();
 
-router.post('/addexpenses' , addExpense);
+router.post('/addexpenses', authMiddleware , addExpense);
 router.delete('/deleteexpenses/:expenseItemId', authMiddleware, deleteExpenseItem); // Route for deleting an expense item
 router.put('/updateexpenses/:expenseItemId', authMiddleware, updateExpenseItem); // Route for updating an expense item
 
