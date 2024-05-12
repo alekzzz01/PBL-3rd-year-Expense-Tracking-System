01542044e071a5d2668cf3f2143d2dbf23627cc9
alekzzz01 <96913449+alekzzz01@users.noreply.github.com>
2024-05-13 03:09:43 +0800

    client production

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ dotenv.config();
   app.use(cookieParser());
   app.use(cors({
     // origin: 'http://localhost:3000', // Allow requests from this origin
-    origin: 'https://expense-client.vercel.app/',
+    origin: 'https://expense-client.vercel.app',
     credentials: true, 
   }));
 
