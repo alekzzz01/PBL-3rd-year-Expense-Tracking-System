116aeb9d339616b2327bee1974851af4a4f85f40
alekzzz01 <96913449+alekzzz01@users.noreply.github.com>
2024-05-13 02:37:26 +0800

    bcryptjs

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,7 +1,7 @@
 import {User} from '../models/User.js'
 import nodemailer from 'nodemailer';
 import jwt from 'jsonwebtoken';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import asyncHandler from '../middleWare/asyncHandler.js';
 
 
