330997c079cb463ac8c474c57910f3e8d51f5e9b
alekzzz01 <96913449+alekzzz01@users.noreply.github.com>
2024-06-01 00:58:24 +0800

    url

--- client/src/store/url.js
+++ client/src/store/url.js
@@ -1,5 +1,5 @@
-// export const baseUrl = 'https://pbl-server-in7x.onrender.com'
- export const baseUrl = 'http://localhost:5000'
+ export const baseUrl = 'https://pbl-server-in7x.onrender.com'
+// export const baseUrl = 'http://localhost:5000'
 
 
 
